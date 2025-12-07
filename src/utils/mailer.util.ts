d542a3967750d7998e10cb5f8d63515f6b5fc0f8
Ademuyiwa Ikotun <ademuyiwaikotun@gmail.com>
2025-12-07 11:04:33 +0100

    Deleted redundant codes on mailer module

--- src/utils/mailer.util.ts
+++ src/utils/mailer.util.ts
@@ -1,27 +1,9 @@
-import nodemailer from "nodemailer";
 import { ServerClient } from "postmark";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 // Create transporter
 const transporter = new ServerClient(process.env.POSTMARK_SERVER_TOKEN!);
-// const transporter = nodemailer.createTransport({
-//   host: process.env.MAIL_HOST,
-//   port: Number(process.env.MAIL_PORT),
-//   secure: false,
-//   auth: {
-//     user: process.env.MAIL_USER,
-//     pass: process.env.MAIL_PASSWORD,
-//   },
-//   connectionTimeout: 10000,
-//   greetingTimeout: 10000,
-//   socketTimeout: 20000,
-//   tls: {
-//     servername: process.env.MAIL_HOST,
-//     minVersion: "TLSv1.2",
-//     rejectUnauthorized: environment === "development",
-//   },
-// });
 
 // Verify transporter
 const verifyMailer = async () => {
